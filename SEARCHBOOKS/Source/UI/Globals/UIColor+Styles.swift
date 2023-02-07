e3f4b320ee4ee4f17364ec22d29212f2cbf01ba6
Ousmane Diagne <13783524+OussouDkr@users.noreply.github.com>
2023-02-07 03:56:13 +0100

    SEARCHBOOK-4: Complete project configuation (#1)
    * SEARCHBOOK-4: Init Podfile
    
    * SEARCHBOOK-4: Complete project configuration

---
+++ SEARCHBOOKS/Source/UI/Globals/UIColor+Styles.swift
@@ -0,0 +1,79 @@
+//
+//  UIColor+Styles.swift
+//  SEARCHBOOKS
+//
+//  Created by Ousmane DIAGNE on 07/02/2023.
+//  Copyright © 2023 Ousmane DIAGNE. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var textColor: UIColor {
+        return Asset.Colors.gray900.color
+    }
+
+    class var action: UIColor {
+        return Asset.Colors.primary.color
+    }
+
+    class var white: UIColor {
+        return Asset.Colors.white.color
+    }
+
+    class var whiteOpacity60: UIColor {
+        return Asset.Colors.white60.color
+    }
+
+    class var separator: UIColor {
+        return Asset.Colors.gray200.color
+    }
+
+    class var destructive: UIColor {
+        return Asset.Colors.error.color
+    }
+
+    class var cellHighlight: UIColor {
+        return Asset.Colors.gray100.color
+    }
+
+    class var buttonHighlight: UIColor {
+        return Asset.Colors.gray100.color
+    }
+
+    class var placeHolderColor: UIColor {
+        return Asset.Colors.gray400.color
+    }
+
+    class var textOnDark: UIColor {
+        return Asset.Colors.white.color
+    }
+
+    class var primaryActionIconTemplateColor: UIColor {
+        return .action
+    }
+
+    class var tableViewHeaderCellBackgroundColor: UIColor {
+        return Asset.Colors.gray200.color
+    }
+
+    class var selectedCellBackground: UIColor {
+        return Asset.Colors.gray200.color
+    }
+
+    class var destructiveDisabled: UIColor {
+        return Asset.Colors.gray400.color
+    }
+
+    class var alternateViewBackgroundColor: UIColor {
+        return Asset.Colors.gray50.color
+    }
+
+    class var viewBackgroundColor: UIColor {
+        return Asset.Colors.white.color
+    }
+    
+    class var primaryColor: UIColor {
+        return Asset.Colors.primary.color
+    }
+}
